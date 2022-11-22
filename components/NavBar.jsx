d6ca99fe167cbc1c1c6e4531ed419307813776c3
Caroline Thostrup <caro342h@stud.kea.dk>
2022-11-22 21:06:19 +0100

    navbar component

---
+++ components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <nav>
+      <h1>Carbon/SoMe</h1>
+    </nav>
+  );
+}
+
+export default NavBar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
+import NavBar from "../components/NavBar";
 import "../styles/globals.css";
 import "../styles/variables.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
