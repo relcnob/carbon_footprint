3b8d85d758106dc4da2ff094ad19033d008badd5
Caroline Thostrup <caro342h@stud.kea.dk>
2022-11-22 22:08:29 +0100

    hero

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
+import Hero from "../components/Hero";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}></div>;
+  return <Hero />;
 }
