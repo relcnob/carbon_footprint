6cbce3cbb4e83b3e0f91857bb78a1f402cd2e2e3
Caroline Thostrup <caro342h@stud.kea.dk>
2022-11-22 21:03:08 +0100

    props+ data added

--- components/DetailResult.jsx
+++ components/DetailResult.jsx
@@ -5,6 +5,10 @@ function DetailResult() {
   return (
     <section className={styles.detailCardList}>
       <DetailResultCard SoMeName="Facebook" TotalCO2="123" />
+      <DetailResultCard SoMeName="Instagram" TotalCO2="23" />
+      <DetailResultCard SoMeName="Twitter" TotalCO2="532" />
+      <DetailResultCard SoMeName="Instagram" TotalCO2="45" />
+      <DetailResultCard SoMeName="Instagram" TotalCO2="2" />
     </section>
   );
 }
