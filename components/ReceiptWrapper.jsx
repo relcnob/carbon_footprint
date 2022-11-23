a2dd9bf46eb12a0073a65eb67d5cb533bdbb43e9
Fryderyk Boncler <f.relcnob@gmail.com>
2022-11-23 18:44:05 +0100

    fixed styling on receipt sections added receipt entry component

--- components/ReceiptWrapper.jsx
+++ components/ReceiptWrapper.jsx
@@ -1,4 +1,5 @@
 import styles from "../modules/Receipt.module.css";
+import ReceiptEntry from "./ReceiptEntry";
 
 function ReceiptWrapper() {
   return (
@@ -8,49 +9,37 @@ function ReceiptWrapper() {
       <span>****************************</span>
       <h2>CARBON FOOTPRINT RECEIPT</h2>
       <span>****************************</span>
-      <section>
-        <table>
-          <tbody>
-            <tr>
-              <th>SoMe platform</th>
-              <th>g of CO2</th>
-            </tr>
-            <tr>
-              <td>Facebook</td>
-              <td>240.33</td>
-            </tr>
-            <tr>
-              <td>Facebook</td>
-              <td>240.33</td>
-            </tr>
-            <tr>
-              <td>Facebook</td>
-              <td>240.33</td>
-            </tr>
-            <tr>
-              <td>Facebook</td>
-              <td>240.33</td>
-            </tr>
-          </tbody>
-        </table>
-        <span>****************************</span>
-        <table>
-          <tbody>
-            <tr>
-              <td>Total</td>
-              <td>sum 12</td>
-            </tr>
-            <tr>
-              <td>Unit</td>
-              <td>g of CO2</td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
+
+      <table>
+        <tbody>
+          <tr>
+            <th>SoMe platform</th>
+            <th>g of CO2</th>
+          </tr>
+          {/* ENTRIES GO HERE props: 'name" and "amount" see example below */}
+          <ReceiptEntry name="facebook" amount="123.45" />
+        </tbody>
+      </table>
+      <span>****************************</span>
+      <table>
+        <tbody>
+          <tr>
+            <td>Total</td>
+            {/* total should be calculated below */}
+            <td>sum 12</td>
+          </tr>
+          <tr>
+            <td>Unit</td>
+            <td>g of CO2</td>
+          </tr>
+        </tbody>
+      </table>
+
       <span>****************************</span>
       <h2>Thank you for testing your digital carbon footprint</h2>
       <span>****************************</span>
       <div className={styles.barcode}></div>
+      {/* UUID goes below */}
       <p>935a4628-5b4b-4378-ac6a-88a4b6508664</p>
     </section>
   );
