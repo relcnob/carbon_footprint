d830ad1e694e7439126079ec4bda1972173cea8d
Caroline Thostrup <caro342h@stud.kea.dk>
2022-11-23 21:50:42 +0100

    fetching data from api

--- pages/result/[slug].js
+++ pages/result/[slug].js
@@ -1,2 +1,29 @@
-fetch("/api/carbon-data/get..");
+import DetailedResults from "../../components/DetailedResults";
+import { useState, useEffect } from "react";
 // fetch serverside props
+
+export default function Profile(props) {
+  console.log(props);
+
+  return (
+    <>
+      <section>
+        <h1>Your Results</h1>
+        {/* <Receipt /> */}´
+      </section>
+      <section>
+        <h2>Detailed Summary</h2>
+        <DetailedResults />
+      </section>
+      <section>
+        <h3>Other users of this website</h3>
+      </section>
+    </>
+  );
+}
+export async function getServerSideProps(context) {
+  const slug = context.params.slug;
+  const res = await fetch(`http://localhost:3000/api/carbon-data/get-overall/${slug}`);
+  const data = await res.json();
+  return { props: data };
+}
