4b44353930866e0521cec59d077707055c66311e
Caroline Thostrup <caro342h@stud.kea.dk>
2022-11-23 14:19:14 +0100

    carbon form

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
-import Head from "next/head";
-import Image from "next/image";
-import Form from "../components/Form";
 import Hero from "../components/Hero";
-import styles from "../styles/Home.module.css";
-
+import CarbonForm from "../components/CarbonForm.jsx";
 export default function Home() {
   return (
     <>
       <Hero />
-      <Form />
+      <CarbonForm />
     </>
   );
 }
