166e0f58ef8cb1524108fffa2bfb586f625b4a7e
Caroline Thostrup <caro342h@stud.kea.dk>
2022-11-24 11:38:55 +0100

    displaying data on detailed section

--- components/DetailedResults.jsx
+++ components/DetailedResults.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import styles from "../styles/DetailedResults.module.css";
 import DetailResultCard from "./DetailResultCard";
-function DetailedResults() {
+function DetailedResults(props) {
+  console.log(props);
   return (
     <section className={styles.detailCardList}>
-      <DetailResultCard SoMeName="Facebook" TotalCO2="123" />
-      <DetailResultCard SoMeName="Instagram" TotalCO2="23" />
-      <DetailResultCard SoMeName="Twitter" TotalCO2="532" />
-      <DetailResultCard SoMeName="Instagram" TotalCO2="45" />
-      <DetailResultCard SoMeName="Instagram" TotalCO2="2" />
+      <DetailResultCard SoMeName="facebook" TotalCO2={props.facebook_carbon} />
+      <DetailResultCard SoMeName="twitter" TotalCO2={props.twitter_carbon} />
+      <DetailResultCard SoMeName="instagram" TotalCO2={props.instagram_carbon} />
+      <DetailResultCard SoMeName="tiktok" TotalCO2={props.tiktok_carbon} />
+      <DetailResultCard SoMeName="youtube" TotalCO2={props.youtube_carbon} />
+      <DetailResultCard SoMeName="pinterest" TotalCO2={props.pinterest_carbon} />
+      <DetailResultCard SoMeName="twitch" TotalCO2={props.twitch_carbon} />
+      <DetailResultCard SoMeName="reddit" TotalCO2={props.reddit_carbon} />
+      <DetailResultCard SoMeName="linkedin" TotalCO2={props.linkedin_carbon} />
+      <DetailResultCard SoMeName="snapchat" TotalCO2={props.snapchat_carbon} />
+      <DetailResultCard SoMeName="whatsapp" TotalCO2={props.whatsapp_carbon} />
+      <DetailResultCard SoMeName="tinder" TotalCO2={props.tinder_carbon} />
     </section>
   );
 }
