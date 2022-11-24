d1c6acf6af1bfed62958ed3a8908239ddd27decd
Caroline Thostrup <caro342h@stud.kea.dk>
2022-11-24 14:08:40 +0100

    adding class and styling

--- pages/result/[slug].js
+++ pages/result/[slug].js
@@ -1,21 +1,21 @@
 import DetailedResults from "../../components/DetailedResults";
-import { useState, useEffect } from "react";
 import ReceiptWrapper from "../../components/ReceiptWrapper";
+import styles from "../../styles/Result.module.css";
 // fetch serverside props
 
 export default function Results(props) {
   return (
     <>
-      <section>
-        <h1>Your Results</h1>
+      <section className={styles.section}>
+        <h1 className={styles.h1}>Your Results</h1>
         <ReceiptWrapper {...props} />
       </section>
-      <section>
-        <h2>Detailed Summary</h2>
+      <section className={styles.section}>
+        <h2 className={styles.h2}>Detailed Summary</h2>
         <DetailedResults {...props} />
       </section>
-      <section>
-        <h3>Other users of this website</h3>
+      <section className={styles.section}>
+        <h3 className={styles.h3}>Other users of this website</h3>
       </section>
     </>
   );
